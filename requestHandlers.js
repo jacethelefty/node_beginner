8db60bb06487e9eccf5ba37f60569508fb456509
jacethelefty <jacemontoya@live.com>
2016-01-11 10:56:54 -0800

    NB Monday Commit

--- requestHandlers.js
+++ requestHandlers.js
@@ -30,6 +30,7 @@ function upload(response, request) {
 
   var form = new formidable.IncomingForm();
   console.log("about to parse");
+
   form.parse(request, function(error, fields, files) {
     console.log("parsing done");
 
@@ -39,8 +40,8 @@ function upload(response, request) {
         fs.rename(files.upload.path, "/tmp/test.png");
       }
     });
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("received image: <br/>");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<h1>received image: <br/></h1>");
     response.write("<img src='/show' />");
     response.end();
   });
